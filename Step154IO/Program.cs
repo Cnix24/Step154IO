8f6840d18f713207fc70f7e20a52859762ba2ee6
Cnix24 <calvinattl24@gmail.com>
2019-07-25 13:53:08 -0600

    Update

---
+++ Step154IO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Step154IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string userInput = Console.ReadLine();
+            File.WriteAllText(@"C:...", userInput);
+
+            string readInput = File.ReadAllText(@"C:...");
+            Console.WriteLine(readInput);
+        }
+    }
+}
